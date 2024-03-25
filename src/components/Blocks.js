c88bc33db581b7c27561c5c721553f21859b1f94
NithinNitz12 <nithinraj10@hotmail.com>
2024-03-24 21:16:26 -0400

    ⚡️ Data fetched from the backend

--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BlockDetails from "./BlockDetails";
 import { faker } from "@faker-js/faker";
 import Header from "./Header";
@@ -13,17 +13,25 @@ for (let i = 0; i < 5; i++) {
 }
 
 function Blocks() {
+  const [ethereumAddresses, setEthereumAddresses] = useState("");
   const [selectedAddress, setSelectedAddress] = useState(null);
   const [selectedBlock, setSelectedBlock] = useState(false);
-  const [balance, setBalance] = useState("");
-  const [gasUsed, setGasUsed] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/blocks/block")
+      .then((res) => res.json())
+      .then((json) => setEthereumAddresses(json))
+      .catch((error) => console.error(error));
+  }, []);
+
+  console.log("ethAddr", ethereumAddresses);
 
   const handleOnChange = (e) => {
     e.preventDefault();
     setSelectedAddress(e.target.value);
     setSelectedBlock(true);
-    setBalance(Math.round(Math.random() * 1000));
-    setGasUsed(Math.round(Math.random() * 10000));
+    // setBalance(Math.round(Math.random() * 1000));
+    // setGasUsed(Math.round(Math.random() * 10000));
   };
 
   return (
@@ -31,18 +39,30 @@ function Blocks() {
       <Header title="Blocks" />
       <FormControl sx={{ m: 1, minWidth: 430 }}>
         <InputLabel>Ethereum Address</InputLabel>
-        <Select label="Ethereum Address" onChange={handleOnChange}>
-          <MenuItem value=""></MenuItem>
-          {address.map((address) => (
-            <MenuItem value={address}>{address}</MenuItem>
-          ))}
-        </Select>
+        {ethereumAddresses === "" ? (
+          <div></div>
+        ) : (
+          <Select label="Ethereum Address" onChange={handleOnChange}>
+            {/* <MenuItem value=""></MenuItem> */}
+            {ethereumAddresses.map((address, index) => (
+              <MenuItem key={index} value={address.address}>
+                {address.address}
+              </MenuItem>
+            ))}
+          </Select>
+        )}
       </FormControl>
       {selectedBlock ? (
         <BlockDetails
           address={selectedAddress}
-          balance={balance}
-          gasUsed={gasUsed}
+          balance={
+            ethereumAddresses.find((block) => block.address === selectedAddress)
+              .balance
+          }
+          gasUsed={
+            ethereumAddresses.find((block) => block.address === selectedAddress)
+              .gasUsed
+          }
         />
       ) : (
         <h1>Ethereum Address is required</h1>
